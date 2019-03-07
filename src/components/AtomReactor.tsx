1502a36b358c9d109a4e7c9c6a49312fafa6dc71
commanderking <jek582@mail.harvard.edu>
2019-03-06 22:11:14 -0500

    get atom moving and canvas resetting correctly

--- src/components/AtomReactor.tsx
+++ src/components/AtomReactor.tsx
@@ -13,34 +13,41 @@ const AtomReactor = ({ addedElements }: Props) => {
     // @ts-ignore
     const context = canvas.current.getContext("2d");
 
+    const atomSize = 15;
     context.save();
     context.scale(1, 1);
     context.fillStyle = "#FFFFFF";
-    context.fillRect(0, 0, 50, 50);
 
     var pos = 0;
     const drawElementOne = () => {
+      const xPos = 25;
+      const yPos = 50;
       // @ts-ignore
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      debugger;
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
       context.strokeStyle = "black";
       context.beginPath();
-      context.arc(25 + pos, 25, 50 / 4, 0, Math.PI * 2);
+      context.arc(25 + pos, 50, atomSize, 0, Math.PI * 2);
       context.stroke();
       context.restore();
 
       context.font = "11px Arial";
-      context.fillText("Hey", 20 + pos, 30);
+      context.fillText("H", xPos - 3 + pos, yPos + 3);
       pos += 1;
     };
 
     drawElementOne();
-    window.setInterval(drawElementOne, 100);
+    window.setInterval(drawElementOne, 30);
   });
   return (
     <div>
       <h1>Reacting...</h1>
-      <canvas ref={canvas} />
+      <canvas
+        ref={canvas}
+        css={css`
+          border: 1px solid black;
+          width: 500px;
+        `}
+      />
     </div>
   );
 };

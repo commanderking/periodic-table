4057f9502b863fd465af630fd22adf6243201247
commanderking <jek582@mail.harvard.edu>
2019-03-13 22:40:51 -0400

    initial refactor of page to use reducers

--- src/examples/ElementGroupExample.tsx
+++ src/examples/ElementGroupExample.tsx
@@ -1,10 +1,58 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import ElementGroup from "../components/ElementGroup";
 import { fetchPeriodicTableDataGroupedByColumn } from "../requests/periodicTable";
 import SelectedElementsPreview from "../components/SelectedElementsPreview";
 import AtomReactor from "../components/AtomReactor";
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
+
+interface State {
+  selectedElement: string;
+  addedElements: Array<any>;
+  isReacting: boolean;
+}
+
+const initialState = {
+  selectedElement: "",
+  addedElements: [],
+  isReacting: false
+};
+
+const reducer = (state: State, action: any) => {
+  switch (action.type) {
+    case "START_NEW_REACTION":
+      return {
+        ...state,
+        selectedElement: "",
+        addedElements: [],
+        isReacting: false
+      };
+    case "SET_SELECTED_ELEMENT":
+      return {
+        ...state,
+        ...action.payload
+      };
+    case "ADD_ELEMENT":
+      return {
+        ...state,
+        addedElements: [...state.addedElements, action.payload.element]
+      };
+    case "UPDATE_ADDED_ELEMENTS":
+      return {
+        ...state,
+        ...action.payload
+      };
+    case "SET_IS_REACTING":
+      return {
+        ...state,
+        ...action.payload
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const TodosDispatch = React.createContext(null);
 
 const ElementGroupExample = () => {
   /* 
@@ -16,19 +64,16 @@ const ElementGroupExample = () => {
     }
   */
   const [elementsByColumn, setElements] = useState({});
-  const [selectedElement, setSelectedElement] = useState("");
-  const [addedElements, updateAddedElements] = useState([]);
-  const [isReacting, setIsReacting] = useState(false);
 
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { selectedElement, addedElements, isReacting } = state;
   const addElement = (element: Object) => {
     // @ts-ignore
-    updateAddedElements([...addedElements, element]);
+    dispatch({ type: "ADD_ELEMENT", payload: { element } });
   };
 
   const startNewReaction = () => {
-    setIsReacting(false);
-    updateAddedElements([]);
-    setSelectedElement("");
+    dispatch({ type: "START_NEW_REACTION", payload: {} });
   };
 
   const removeElement = (element: any) => {
@@ -36,11 +81,23 @@ const ElementGroupExample = () => {
       return element.name === addedElement.name;
     });
 
-    const updatedAddedElements = addedElements.filter((element, index) => {
-      return index !== indexToDelete;
+    const updatedAddedElements = addedElements.filter(
+      (element: any, index: number) => {
+        return index !== indexToDelete;
+      }
+    );
+    dispatch({
+      type: "UPDATE_ADDED_ELEMENTS",
+      payload: { addedElements: updatedAddedElements, selectedElement: "" }
     });
-    updateAddedElements(updatedAddedElements);
-    setSelectedElement("");
+  };
+
+  const setSelectedElement = (selectedElement: string) => {
+    dispatch({ type: "SET_SELECTED_ELEMENT", payload: { selectedElement } });
+  };
+
+  const setIsReacting = (isReacting: boolean) => {
+    dispatch({ type: "SET_IS_REACTING", payload: { isReacting } });
   };
 
   const fetchDataAndUpdateElementsState = async () => {
@@ -64,7 +121,7 @@ const ElementGroupExample = () => {
       <ElementGroup
         // @ts-ignore
         elements={elementsByColumn[1] || []}
-        selectedElement={selectedElement}
+        selectedElement={state.selectedElement}
         setElement={setSelectedElement}
         addElement={addElement}
         hasSelectedMaxElements={hasSelectedMaxElements}

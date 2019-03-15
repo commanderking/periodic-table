1d955a02fff3722848b8620ebc5a48e5b7212765
commanderking <jek582@mail.harvard.edu>
2019-03-15 18:52:53 -0400

    start completed reaction section

---
+++ src/components/CompletedReactions.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type reaction = { elements: Array<string>; reactionResult: string };
+
+const CompletedReactions = (reactions: Array<reaction>) => {
+  // map over the reactions and show them in a list
+  // info we need:
+  return {};
+};
+
+export default CompletedReactions;
